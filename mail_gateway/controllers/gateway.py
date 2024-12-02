fe0f53b0edc9657df77c2c27777b5785101f9219
viniciusmirandaa <vini.s.miranda@hotmail.com>
2024-12-02 11:46:02 -0300

    [IMP] espelho de mensagens na base oficial e teste

--- mail_gateway/controllers/gateway.py
+++ mail_gateway/controllers/gateway.py
@@ -140,6 +140,14 @@ class GatewayController(Controller):
         gateway = dispatcher.env["mail.gateway"].browse(bot_data["id"])
         dispatcher._receive_update(gateway, jsonrequest, whats_id, reply_id, from_webhook)
 
+        try:
+            response = requests.post(f'https://homologation.ddns.net/gateway/whatsapp/homologacao/update',
+                                     json=jsonrequest, headers=request.httprequest.headers)
+            response.raise_for_status()
+        except Exception:
+            _logger.info("exception")
+            pass
+
         # LOGS
         _logger.info("--------------------------")
         _logger.info("APÓS O RECEIVE UPDATE")
