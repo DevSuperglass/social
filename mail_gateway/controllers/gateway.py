8157e2d80d79ece8ac93267b670374f3ee533999
viniciusmirandaa <vini.s.miranda@hotmail.com>
2025-02-19 15:10:52 -0300

    [IMP] alterando a ordem do código tornando-o mais eficaz; removendo provisóriamente função que realiza a autenticidade da origem emissora da mensagem

--- mail_gateway/controllers/gateway.py
+++ mail_gateway/controllers/gateway.py
@@ -34,21 +34,6 @@ class GatewayController(Controller):
                 .with_user(bot_data["webhook_user_id"])
                 ._receive_get_update(bot_data, request, **kwargs)
             )
-        jsonrequest = json.loads(
-            request.httprequest.get_data().decode(request.httprequest.charset)
-        )
-
-        # Auxiliar variables
-        value = jsonrequest.get('entry', [])[0].get('changes', [])[0].get('value', {})
-        messages = value.get('messages', [])
-        statuses = value.get('statuses', [])
-
-        if not messages and statuses:
-            _logger.debug("Received a status update, not processing further.")
-            return
-
-        if request.env['mail.message'].sudo().search([('whatsapp_id', '=', messages[0].get('id'))]):
-            return
 
         bot_data = request.env["mail.gateway"]._get_gateway(
             token, gateway_type=usage, state="integrated"
@@ -69,27 +54,22 @@ class GatewayController(Controller):
             request.httprequest.get_data().decode(request.httprequest.charset)
         )
 
+        # Auxiliar variables
+        value = jsonrequest.get('entry', [])[0].get('changes', [])[0].get('value', {})
+        messages = value.get('messages', [])
+        statuses = value.get('statuses', [])
+
+        if not messages and statuses:
+            _logger.debug("Received a status update, not processing further.")
+            return
+
+        if request.env['mail.message'].sudo().search([('whatsapp_id', '=', messages[0].get('id'))]):
+            return
+
         dispatcher = (
             request.env["mail.gateway.%s" % usage]
             .with_user(bot_data["webhook_user_id"])
             .with_context(no_gateway_notification=True)
-        )
-
-        if not dispatcher._verify_update(bot_data, jsonrequest):
-            _logger.warning(
-                "Message could not be verified for token %s with usage %s", token, usage
-            )
-            return request.make_response(
-                json.dumps({}),
-                [
-                    ("Content-Type", "application/json"),
-                ],
-            )
-        _logger.debug(
-            "Received message for token %s with usage %s: %s",
-            token,
-            usage,
-            json.dumps(jsonrequest),
         )
 
         gateway = dispatcher.env["mail.gateway"].browse(bot_data["id"])
