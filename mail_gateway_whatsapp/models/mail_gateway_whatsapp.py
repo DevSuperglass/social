c2d95ac2cf2d87de1de67e88cbc3e7ab890fc674
vinícius <61062656+viniciusmirandaa@users.noreply.github.com>
2025-03-31 22:10:04 -0300

    [IMP] adicionando limite na pesquisa de res_partner_gateway_channel na recepção de uma mensagem

--- mail_gateway_whatsapp/models/mail_gateway_whatsapp.py
+++ mail_gateway_whatsapp/models/mail_gateway_whatsapp.py
@@ -398,7 +398,7 @@ class MailGatewayWhatsappService(models.AbstractModel):
                 [
                     ("gateway_id", "=", gateway.id),
                     ("gateway_token", "=", str(author_id)),
-                ]
+                ], limit=1
             )
             if gateway_partner:
                 return gateway_partner.partner_id
