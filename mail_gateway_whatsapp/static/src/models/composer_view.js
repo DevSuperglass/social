7b9d59c8e8bbb870ea837b232f112eafa6cf0a1d
Erick Fernando Martins Santos <erickmartinslima3@gmail.com>
2025-07-08 17:17:36 -0300

    [FIX] alterando para ser possivel responder mensagem, mensagem com erro, a resolver

--- mail_gateway_whatsapp/static/src/models/composer_view.js
+++ mail_gateway_whatsapp/static/src/models/composer_view.js
@@ -1,53 +1,68 @@
-
 /** @odoo-module **/
-
-
 import { registerPatch } from "@mail/model/model_core";
+import { clear, link } from '@mail/model/model_field_command';
+import session from "web.session";
 
 registerPatch({
     name: "ComposerView",
     recordMethods: {
         async postMessage() {
             const composer = this.composer;
-            const originalPostData = structuredClone(this._getMessageData());
-            const originalAttachmentIds = originalPostData.attachment_ids || [];
-
+            const postData = this._getMessageData();
             const params = {
+                'post_data': postData,
                 'thread_id': composer.thread.id,
                 'thread_model': composer.thread.model,
             };
             try {
                 composer.update({ isPostingMessage: true });
-                if (originalAttachmentIds.length > 0) {
-                    for (let i = 0; i < originalAttachmentIds.length; i++) {
-                        const postDataForThisMessage = { ...originalPostData };
-                        postDataForThisMessage.attachment_ids = [originalPostData.attachment_ids[i]];
-                        postDataForThisMessage.attachment_tokens = [originalPostData.attachment_tokens[i]];
-                        params.post_data = postDataForThisMessage;
-                        await this._sendMessage(params);
+                if (composer.thread.model === 'mail.channel') {
+                    Object.assign(postData, {
+                        subtype_xmlid: 'mail.mt_comment',
+                    });
+                } else {
+                    Object.assign(postData, {
+                        subtype_xmlid: composer.isLog ? 'mail.mt_note' : 'mail.mt_comment',
+                    });
+                    if (!composer.isLog) {
+                        params.context = { mail_post_autofollow: this.composer.activeThread.hasWriteAccess };
+                    }
+                }
+                if (this.threadView && this.threadView.replyingToMessageView && this.threadView.thread !== this.messaging.inbox.thread) {
+                    postData.parent_id = this.threadView.replyingToMessageView.message.id;
+                }
+                params.context = Object.assign(params.context || {}, session.user_context);
+                const { threadView = {} } = this;
+                const chatter = this.chatter;
+                const { thread: chatterThread } = this.chatter || {};
+                const { thread: threadViewThread } = threadView;
+                // Keep a reference to messaging: composer could be
+                // unmounted while awaiting the prc promise. In this
+                // case, this would be undefined.
+                if (postData.attachment_ids.lenght > 0) {
+                    for (let i = 0; i < postData.attachment_ids.lenght; i++) {
+                        const postDataAttachments = { ...postData };
+                        postDataAttachments.attachment_ids = [postData.attachment_ids[i]];
+                        postDataAttachments.attachment_tokens = [postData.attachment_tokens[i]];
+                        params.postData = postDataAttachments;
+                        await this._sendMessage(params, chatter, chatterThread, threadViewThread, composer, threadView);
                     }
                 } else {
-                    params.post_data = originalPostData;
-                    await this._sendMessage(params);
+                    await this._sendMessage(params, chatter, chatterThread, threadViewThread, composer, threadView);
                 }
-                if (composer.exists()) {
-                    composer._reset();
-                }
-            } catch (error) {
-                if (composer.exists()) {
-                    composer.update({ isPostingMessage: false });
-                }
-                throw error;
             } finally {
-                if (composer.exists() && composer.isPostingMessage) {
+                if (composer.exists()) {
                     composer.update({ isPostingMessage: false });
                 }
             }
         },
-
-        async _sendMessage(params) {
+        async _sendMessage(params,  chatter, chatterThread, threadViewThread, composer, threadView) {
+            // This helper function contains the logic to send one message and handle its response.
+            // This avoids duplicating the response-handling code inside the loop.
             const messaging = this.messaging;
             const messageData = await this.messaging.rpc({ route: `/mail/message/post`, params });
+
+            // This part is mostly from your original code, to handle UI updates
             if (!messaging.exists()) {
                 return;
             }
@@ -58,6 +73,8 @@ registerPatch({
                 threadView.update({ hasAutoScrollOnMessageReceived: true });
                 threadView.addComponentHint('message-posted', { message });
             }
-        },
+            // ... include other logic from your original response handling if needed ...
+        }
+
     }
 });
