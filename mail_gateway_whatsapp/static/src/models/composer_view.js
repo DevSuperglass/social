88616f6ab7b7a708a658bba3bca149225c990318
Erick Fernando Martins Santos <erickmartinslima3@gmail.com>
2025-07-10 14:47:39 -0300

    [FIX] colocando logica em uma função somente, erros resolvidos

--- mail_gateway_whatsapp/static/src/models/composer_view.js
+++ mail_gateway_whatsapp/static/src/models/composer_view.js
@@ -8,73 +8,111 @@ registerPatch({
     recordMethods: {
         async postMessage() {
             const composer = this.composer;
-            const postData = this._getMessageData();
+            const originalPostData = this._getMessageData();
+
             const params = {
-                'post_data': postData,
                 'thread_id': composer.thread.id,
                 'thread_model': composer.thread.model,
             };
+
             try {
                 composer.update({ isPostingMessage: true });
+
                 if (composer.thread.model === 'mail.channel') {
-                    Object.assign(postData, {
-                        subtype_xmlid: 'mail.mt_comment',
-                    });
+                    Object.assign(originalPostData, { subtype_xmlid: 'mail.mt_comment' });
                 } else {
-                    Object.assign(postData, {
-                        subtype_xmlid: composer.isLog ? 'mail.mt_note' : 'mail.mt_comment',
-                    });
+                    Object.assign(originalPostData, { subtype_xmlid: composer.isLog ? 'mail.mt_note' : 'mail.mt_comment' });
                     if (!composer.isLog) {
                         params.context = { mail_post_autofollow: this.composer.activeThread.hasWriteAccess };
                     }
                 }
-                if (this.threadView && this.threadView.replyingToMessageView && this.threadView.thread !== this.messaging.inbox.thread) {
-                    postData.parent_id = this.threadView.replyingToMessageView.message.id;
+                if (this.threadView && this.threadView.replyingToMessageView) {
+                    originalPostData.parent_id = this.threadView.replyingToMessageView.message.id;
                 }
                 params.context = Object.assign(params.context || {}, session.user_context);
+
                 const { threadView = {} } = this;
                 const chatter = this.chatter;
                 const { thread: chatterThread } = this.chatter || {};
                 const { thread: threadViewThread } = threadView;
-                // Keep a reference to messaging: composer could be
-                // unmounted while awaiting the prc promise. In this
-                // case, this would be undefined.
-                if (postData.attachment_ids.lenght > 0) {
-                    for (let i = 0; i < postData.attachment_ids.lenght; i++) {
-                        const postDataAttachments = { ...postData };
-                        postDataAttachments.attachment_ids = [postData.attachment_ids[i]];
-                        postDataAttachments.attachment_tokens = [postData.attachment_tokens[i]];
-                        params.postData = postDataAttachments;
-                        await this._sendMessage(params, chatter, chatterThread, threadViewThread, composer, threadView);
+
+                const messaging = this.messaging;
+                const originalAttachmentIds = originalPostData.attachment_ids || [];
+                const originalAttachmentTokens = originalPostData.attachment_tokens || [];
+
+                const messageDatas = [];
+
+                if (originalAttachmentIds.length > 0) {
+                    for (let i = 0; i < originalAttachmentIds.length; i++) {
+                        const postDataForThisMessage = { ...originalPostData };
+                        postDataForThisMessage.attachment_ids = [originalAttachmentIds[i]];
+                        postDataForThisMessage.attachment_tokens = [originalAttachmentTokens[i]];
+                        if (i > 0) {
+                            postDataForThisMessage.body = "";
+                        }
+                        params.post_data = postDataForThisMessage;
+                        const messageData = await messaging.rpc({ route: `/mail/message/post`, params });
+                        messageDatas.push(messageData);
                     }
                 } else {
-                    await this._sendMessage(params, chatter, chatterThread, threadViewThread, composer, threadView);
+                    params.post_data = originalPostData;
+                    const messageData = await messaging.rpc({ route: `/mail/message/post`, params });
+                    messageDatas.push(messageData);
                 }
+
+                for (const messageData of messageDatas) {
+                    const message = messaging.models['Message'].insert(
+                        messaging.models['Message'].convertData(messageData)
+                    );
+                    if (messaging.hasLinkPreviewFeature && !message.isBodyEmpty) {
+                        messaging.rpc({
+                            route: `/mail/link_preview`,
+                            params: {
+                                message_id: message.id
+                            }
+                        }, { shadow: true });
+                    }
+                    for (const threadView of message.originThread.threadViews) {
+                        threadView.update({ hasAutoScrollOnMessageReceived: true });
+                        threadView.addComponentHint('message-posted', { message });
+                    }
+                    if (chatter && chatter.exists() && chatter.hasParentReloadOnMessagePosted && messageData.recipients.length) {
+                        chatter.reloadParentView();
+                    }
+                    if (chatterThread) {
+                        if (this.exists()) {
+                            this.delete();
+                        }
+                        if (chatterThread.exists()) {
+                            chatterThread.fetchData(['followers', 'messages', 'suggestedRecipients']);
+                        }
+                    }
+                    if (threadViewThread) {
+                        if (threadViewThread === messaging.inbox.thread) {
+                            messaging.notify({
+                                message: sprintf(messaging.env._t(`Message posted on "%s"`), message.originThread.displayName),
+                                type: 'info',
+                            });
+                            if (this.exists()) {
+                                this.delete();
+                            }
+                        }
+                        if (threadView && threadView.exists()) {
+                            threadView.update({ replyingToMessageView: clear() });
+                        }
+                    }
+                }
+
+                if (composer.exists()) {
+                    composer._reset();
+                }
+
             } finally {
                 if (composer.exists()) {
                     composer.update({ isPostingMessage: false });
                 }
             }
         },
-        async _sendMessage(params,  chatter, chatterThread, threadViewThread, composer, threadView) {
-            // This helper function contains the logic to send one message and handle its response.
-            // This avoids duplicating the response-handling code inside the loop.
-            const messaging = this.messaging;
-            const messageData = await this.messaging.rpc({ route: `/mail/message/post`, params });
-
-            // This part is mostly from your original code, to handle UI updates
-            if (!messaging.exists()) {
-                return;
-            }
-            const message = messaging.models['Message'].insert(
-                messaging.models['Message'].convertData(messageData)
-            );
-            for (const threadView of message.originThread.threadViews) {
-                threadView.update({ hasAutoScrollOnMessageReceived: true });
-                threadView.addComponentHint('message-posted', { message });
-            }
-            // ... include other logic from your original response handling if needed ...
-        }
 
     }
 });
